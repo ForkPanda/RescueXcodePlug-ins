5eafbbd9cd6df5a0dc1db2a6f00d3255c011646f
Ricky <forkpanda@gmail.com>
2015-11-27 14:22:11 +0800

    For all version of Xcode without args

--- RescueXcode.sh
+++ RescueXcode.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+# !/bin/sh
 echo '
     _/_/_/                              _/            _/    _/  _/    _/      
    _/    _/    _/_/_/  _/_/_/      _/_/_/    _/_/_/  _/  _/        _/_/_/_/   
@@ -7,19 +7,17 @@ echo '
 _/          _/_/_/  _/    _/    _/_/_/    _/_/_/  _/    _/  _/      _/_/                                                                        
 '
 
-v=$1
-if [ "$v" == "" ] ;then
-    v=7.1.1
-fi
-
-echo "Try to rescue your Xcode " $v "..."
-
 find ~/Library/Application\ Support/Developer/Shared/Xcode/Plug-ins -name Info.plist -maxdepth 3 | xargs -I{} defaults write {} DVTPlugInCompatibilityUUIDs -array-add defaults read /Applications/Xcode.app/Contents/Info.plist DVTPlugInCompatibilityUUID
 
-defaults delete com.apple.dt.Xcode DVTPlugInManagerNonApplePlugIns-Xcode-$v
+ls=`defaults read com.apple.dt.Xcode | grep -o 'DVTPlugInManagerNonApplePlugIns-Xcode-[^\"]*'`
 
-if [ $? -eq 0 ];then
-  echo "\nRescue Success!Restart your Xcode and choose 'Load Bundles' option"
+for x in $ls; do
+    echo "Try to rescue your Xcode" ${x##*-} "..."
+    defaults delete com.apple.dt.Xcode $x
+done
+echo $!
+if [ $ls ];then
+  echo "\nRescue Success!Restart your Xcode and choose 'Load Bundles' option!"
 else
-  echo "\nRescue Failed!"
+  echo "\nRescue Failed!Cannot find any config of Plug-ins in your Library!"
 fi
