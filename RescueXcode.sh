0f07c17671d3a248b0bef6325109c97390927212
Ricky <forkpanda@gmail.com>
2015-12-09 22:28:26 +0800

    ༼ つ ◕_◕ ༽つ	༼ つ ◕_◕ ༽つ	༼ つ ◕_◕ ༽つ

--- RescueXcode.sh
+++ RescueXcode.sh
@@ -7,7 +7,7 @@ echo '
 _/          _/_/_/  _/    _/    _/_/_/    _/_/_/  _/    _/  _/      _/_/                                                                        
 '
 
-find ~/Library/Application\ Support/Developer/Shared/Xcode/Plug-ins -name Info.plist -maxdepth 3 | xargs -I{} defaults write {} DVTPlugInCompatibilityUUIDs -array-add defaults read /Applications/Xcode.app/Contents/Info.plist DVTPlugInCompatibilityUUID
+find ~/Library/Application\ Support/Developer/Shared/Xcode/Plug-ins -name Info.plist -maxdepth 3 | xargs -I{} defaults write {} DVTPlugInCompatibilityUUIDs -array-add `defaults read /Applications/Xcode.app/Contents/Info.plist DVTPlugInCompatibilityUUID`
 
 ls=`defaults read com.apple.dt.Xcode | grep -o 'DVTPlugInManagerNonApplePlugIns-Xcode-[^\"]*'`
 
@@ -23,10 +23,10 @@ for x in $ls; do
 
 done
 
-if [ ! ${#ls[@]} -eq 0 ];then
+if [ "$ls" != "" ];then
   echo "\nRescue Success!Restart your Xcode and choose 'Load Bundles' option!"
 else
   echo "\nError!Cannot find any config of Plug-ins in your Library!"
-  echo "You have been run the script maybe."
-  echo "Try to restart Xcode and check wheather it works."
+  echo "* You have been run the script maybe."
+  echo "* Try to restart Xcode and check wheather it works."
 fi
